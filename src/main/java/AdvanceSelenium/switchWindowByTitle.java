dc8d91dcee248d2dc54e97e1ee4509f1f46eb19e
Subah <subahs301@gmail.com>
2024-08-04 13:03:29 -0400

    practice MR

--- src/main/java/AdvanceSelenium/switchWindowByTitle.java
+++ src/main/java/AdvanceSelenium/switchWindowByTitle.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class switchWindowByTitle {
 
+    //creating the project MR
 
     public static void main (String[] args) {
         WebDriver driver;
